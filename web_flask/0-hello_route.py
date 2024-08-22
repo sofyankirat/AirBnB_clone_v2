c8e51a8be0a0c39692c702ab6dafbf9ee5cd3c9f
sofyankirat <www.sofyankirat123@gmail.com>
2024-08-22 18:05:25 +0000

    sdcs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
+"""starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+#Define the route for the root URL '/'
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB! """
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
